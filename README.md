6228b4d6110ebd0e5ef0188a623efae66efdd78f
nguyenyou <tunt081295@gmail.com>
2025-10-01 11:30:34 +0700

    🚢 ship it

--- README.md
+++ README.md
@@ -1,20 +1,44 @@
-# Mill Vite
+There are three types of variance annotations in Scala:
 
-## Development
+1. Covariance (+)
 
-```sh
-./mill www.fullLinkJS
-yarn install
-yarn dev
+```scala
+trait Tag[+El <: ReactiveElement]
 ```
 
+Meaning: If `A` is a subtype of `B`, then `Tag[A]` is a subtype of `Tag[B]`
+Use case: When the type parameter is only used in "output" positions (return types)
+Example: `List[+A]` - you can assign `List[Dog]` to `List[Animal]`
 
-## Tests
+2. Contravariance (-)
 
-```sh
-./mill www.test
+```scala
+trait Processor[-El <: ReactiveElement]
 ```
 
-## Mill Versions
+Meaning: If `A` is a subtype of `B`, then `Processor[B]` is a subtype of `Processor[A]` (reversed!)
+Use case: When the type parameter is only used in "input" positions (method parameters)
+Example: `Function1[-T, +R]` - a function that accepts `Animal` can be used where `Dog` is expected
 
-https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/
+3. Invariance (no annotation)
+
+```scala
+trait Container[El <: ReactiveElement]
+```
+
+Meaning: No subtype relationship - `Container[A]` and `Container[B]` are unrelated even if `A <: B`
+Use case: When the type is used in both input and output positions (mutable collections)
+Example: `Array[T]` - you cannot assign `Array[Dog]` to `Array[Animal]`
+
+Quick comparison:
+
+```scala
+// Covariant (+): subtype relationship preserved
+List[Dog] <: List[Animal] ✓
+
+// Contravariant (-): subtype relationship reversed  
+Processor[Animal] <: Processor[Dog] ✓
+
+// Invariant (none): no relationship
+Array[Dog] and Array[Animal] are unrelated
+```
--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -10,3 +10,56 @@ case class App() {
     )
   }
 }
+
+object App {
+  /*
+  Without the + (covariant), the assignment val baseTag: Tag[ReactiveElement.Base] = divTag would fail, and you couldn't pass a Tag[DivElement] to a function expecting Tag[ReactiveElement.Base].
+The + makes the trait more flexible by preserving the subtype relationship through the type parameter.
+  The key point: because of the + covariance, you can pass Tag[DivElement] or Tag[SpanElement] to a function expecting Tag[ReactiveElement], making the API much more flexible!
+   */
+  trait Tag[+El <: ReactiveElement] {
+
+    val name: String
+
+    val void: Boolean
+
+    /** The string returned by `element.tagName` in JS DOM. It's uppercase for
+      * HTML elements, and case-preserving for SVG elements.
+      */
+    def jsTagName: String
+  }
+
+  class ReactiveElement
+  class DivElement extends ReactiveElement
+  class SpanElement extends DivElement
+
+  object DivTag extends Tag[DivElement] {
+    val name: String = "div"
+    val void: Boolean = false
+    def jsTagName: String = "DIV"
+  }
+
+  object SpanTag extends Tag[SpanElement] {
+    val name: String = "span"
+    val void: Boolean = false
+    def jsTagName: String = "SPAN"
+  }
+
+  val divTag: Tag[DivElement] = DivTag
+  val spanTag: Tag[SpanElement] = SpanTag
+  val generalTag1: Tag[DivElement] = spanTag // ✓ SpanElement <: DivElement
+  val generalTag2: Tag[ReactiveElement] =
+    divTag // ✓ DivElement <: ReactiveElement
+  val generalTag3: Tag[ReactiveElement] =
+    spanTag // ✓ SpanElement <: ReactiveElement
+
+// Practical use case - function that accepts any tag:
+  def processTags(tag: Tag[ReactiveElement]): Unit = {
+    println(s"Processing tag: ${tag.name} (${tag.jsTagName})")
+  }
+
+  def run() = {
+    processTags(DivTag) // ✓ Works!
+    processTags(SpanTag) // ✓ Works!
+  }
+}
